01763c5081a3d360914c9bda70fb89a439cff619
Fazel <fazel20221998@gmail.com>
2024-06-17 22:03:52 +0330

    Update hw_4.3.py

--- hw_4.3.py
+++ hw_4.3.py
@@ -1,12 +1,8 @@
 def is_armstrong(number):
-    # Convert the number to a string to iterate over each digit
-    digits = str(number)
-    # Create a generator expression for the sum of the cubes of its digits
-    armstrong_sum = sum(int(digit) ** 3 for digit in digits)
-    # Check if the sum of the cubes of the digits is equal to the number itself
-    return armstrong_sum == number
 
-# Test the function with a few examples
+    digits = str(number)
+    armstrong_sum = sum(int(digit) ** 3 for digit in digits)
+    return armstrong_sum == number
 numbers_to_test = [153, 370, 371, 407, 123, 9474,3333]
 
 for num in numbers_to_test:
